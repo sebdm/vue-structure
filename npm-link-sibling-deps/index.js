226e60b440201b61e3dfb38b248826477ba27cba
Sebastian Lind <sebastian.lind@gmail.com>
2017-11-22 12:40:43 +0100

    npm-link-sibling-packages: add support for starting out in one of the sibling directories (will only link packages related to the starting sibling)

--- npm-link-sibling-deps/index.js
+++ npm-link-sibling-deps/index.js
@@ -5,21 +5,24 @@ const path = require("path");
 const fs = require("fs");
 
 const startingDirectory = process.cwd()
+const startingInSibling = fs.existsSync(path.join(process.cwd(), "package.json"))
+const startingInName = startingDirectory.split(path.sep)[startingDirectory.split(path.sep).length - 1]
+const topDirectory = startingInSibling ? path.join(startingDirectory, '..') : startingDirectory
 
 // create the packages object
 const packages = fs
-  .readdirSync(process.cwd())
-  .filter(file => fs.statSync(path.join(process.cwd(), file)).isDirectory())
+  .readdirSync(topDirectory)
+  .filter(file => fs.statSync(path.join(topDirectory, file)).isDirectory())
   .filter(file => {
-    return fs.existsSync(path.join(process.cwd(), file, "package.json"));
+    return fs.existsSync(path.join(topDirectory, file, "package.json"));
   })
   .map(file => {
     return {
       name: file,
-      path: path.join(process.cwd(), file),
+      path: path.join(topDirectory, file),
       siblingDeps: [],
       packageJson: JSON.parse(
-        fs.readFileSync(path.join(process.cwd(), file, "package.json"))
+        fs.readFileSync(path.join(topDirectory, file, "package.json"))
       )
     };
   })
@@ -27,14 +30,24 @@ const packages = fs
     return Object.assign(o, { [package.name]: package })
   }, {});
 
-// iterate packages and run npm link for each, plus map sibling deps
 for (let name in packages) {
-  const package = packages[name]
+  if (startingInSibling) {
+    if (name === startingInName) {
+      linkPackage(packages[name])
+      break
+    }
+  } else {
+    linkPackage(packages[name])
+  }
+}
+
+function linkPackage(package) {
   const deps = package.packageJson.dependencies
   const devDeps = package.packageJson.devDependencies
   if (deps) {
     for (let key in deps) {
       if (packages[key]) {
+        linkPackage(packages[key])
         package.siblingDeps.push(key)
       }
     }
@@ -43,24 +56,18 @@ for (let name in packages) {
   if (devDeps) {
     for (let key in devDeps) {
       if (packages[key]) {
+        linkPackage(packages[key])
         package.siblingDeps.push(key)
       }
     }
   }
 
   shelljs.cd(package.path)
-  shelljs.exec(`npm link`)
-  shelljs.cd(startingDirectory)
-}
-
-// iterate packages and run npm link $siblingDep for each sibling dep
-for (let name in packages) {
-  const package = packages[name]
-  shelljs.cd(package.path)
   package.siblingDeps.forEach(dep => {
     shelljs.exec(`npm link ${dep}`)
     console.log(`linking ${dep} for ${package.name}`)
   })
 
+  shelljs.exec(`npm link`)
   shelljs.cd(startingDirectory)
 }
